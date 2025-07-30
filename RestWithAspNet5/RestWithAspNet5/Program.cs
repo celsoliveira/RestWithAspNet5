28ca06d4c0d8a8aefb475143f213fbd48201b950
Celso de Oliveira Silva <celsoliveira@gmail.com>
2025-07-29 22:25:08 -0300

    Seção 8 - Integrando a APlicação ao banco de dados

--- RestWithAspNet5/RestWithAspNet5/Program.cs
+++ RestWithAspNet5/RestWithAspNet5/Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithAspNet5.Model.Context;
+using RestWithAspNet5.Services;
+using RestWithAspNet5.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
+var appName = "REST API's RESTful from 0 to Azure with ASP.NET Core 8 and Docker";
+var appVersion = "v1";
+var appDescription = $"REST API RESTful developed in course '{appName}'";
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Conexão com banco de dados
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
+
+
+// adicionando services para injeção de dependência
+builder.Services.AddScoped<IPersonService, PersonService>();
 
 var app = builder.Build();
 
---
+++ RestWithAspNet5/RestWithAspNet5/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAspNet5.Model;
+
+namespace RestWithAspNet5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
